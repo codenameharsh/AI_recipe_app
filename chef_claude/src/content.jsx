ed100e6b07e635394853041b85335f2421cbbeb0
codenameharsh <harshitha167@gmail.com>
2025-01-12 22:25:22 -0800

    adding functionality with useState

--- chef_claude/src/content.jsx
+++ chef_claude/src/content.jsx
@@ -1,15 +1,35 @@
+import React from 'react'
+
+
 export default function Content(){
+    const [ingredients, setIngredients] = React.useState([])
+    
+    const ingredientsListItems = ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
+
+    function handleSubmit(event) {
+        setIngredients(prevIngredient => [...prevIngredient, newIngredient]);
+
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+    }
+
     return (
         <main>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input 
                   type="text"
                   aria-label="Add ingredient"
                   placeholder="eg: Basil"
+                  name="ingredient"
                 />
                 <button className="ingredient_btn">+ Add Ingredient</button>
-
             </form>
+            <ul>
+                {ingredientsListItems}
+            </ul>
         </main>
     )
 }
